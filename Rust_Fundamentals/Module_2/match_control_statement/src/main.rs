cd5c0b3e2e3208c1dff8c5b410f98f032213b64f
ngodoy <tundraorca.tech@gmail.com>
2025-03-01 13:53:01 +0000

    match control statement

--- Rust_Fundamentals/Module_2/match_control_statement/src/main.rs
+++ Rust_Fundamentals/Module_2/match_control_statement/src/main.rs
@@ -2,12 +2,12 @@ use std::io::{self, Write}; // Import Write for flushing
 
 fn main() {
 
-    let mut i_name = String::new();
+    let mut i_name: String = String::new();
     
     print!("Please, enter your name: ");
     
     io::stdout().flush().unwrap(); // Ensure the prompt appears before input
-    io::stdin().read_line(&mut i_name).expect("Failed to read line");
+    io::stdin().read_line(buf: &mut i_name).expect(msg: "Failed to read line");
 
     match i_name.trim() {
         "Alice" => println!("Hello, Alice!"),
