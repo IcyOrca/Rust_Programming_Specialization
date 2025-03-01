e41a6c40722ed9b7a3415e06099b50d4f1463df5
ngodoy <tundraorca.tech@gmail.com>
2025-03-01 15:24:05 +0000

    Simple Unit Function

--- Rust_Fundamentals/Module_2/Function_Basics/Simple_Unit_Functions/src/main.rs
+++ Rust_Fundamentals/Module_2/Function_Basics/Simple_Unit_Functions/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+fn main(){
+    let numbers : Vec<i32> = vec![1, 2, 3, 4, 5];
+    process_numbers(&numbers);
+}
+
+
+fn process_numbers(numbers : &Vec<i32>) {
+    let mut sum : i32 = 0;
+
+    for number in numbers {
+        sum += number;
+    }
+
+    println!("Sum of numbers: {}", sum);
+
+    if sum % 2 == 0 {
+        println!("Sum is even");
+    } else {
+        println!("Sum is odd");
+    }
 }
